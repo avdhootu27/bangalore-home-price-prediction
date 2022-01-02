18f3e69aea1e665352c42712a8dcc966cb1bf5a5
Avdhoot <avdhootu.27@gmail.com>
2022-01-02 15:53:20 +0530

    Get locations done

--- server/server.py
+++ server/server.py
@@ -1,5 +1,14 @@
 from flask import Flask, request, jsonify
+import util
 app = Flask(__name__)
+
+@app.route('/get_location_names')
+def get_location_names():
+    response = jsonify({
+        "locations": util.get_location_names()
+    })
+    response.headers.add('Access-control-allow-origin', '*')
+    return response
 
 if __name__ == "__main__":
     print("Starting Python Flask Server For Home Price Prediction...")
---
+++ server/util.py
@@ -0,0 +1,24 @@
+import json
+import pickle
+
+__locations = None
+__data_columns = None
+__model = None
+
+def get_location_names():
+    return __locations
+
+def load_saved_artifacts():
+    global __locations
+    global __data_columns
+
+    with open("./artifacts/columns.json", 'r') as f:
+        __data_columns = json.load(f)['data_columns']
+        __locations = __data_columns[3:]
+
+    with open("./artifacts/banglore_home_prices_model.pickle", 'rb') as f:
+        __model = pickle.load(f)
+
+if __name__ == "__main__":
+    load_saved_artifacts()
+    print(get_location_names())
